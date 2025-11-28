f2ee2c6aed9abb7f0481286f43ffed2faae0d373
gabrielbaute <gabrielbaute@gmail.com>
2025-11-27 20:56:20 -0400

    Clase de datos de respuesta de fourier

---
+++ app/fourier/component.py
@@ -0,0 +1,28 @@
+# fourier/component.py
+import cmath
+
+class FourierComponent:
+    def __init__(self, k: int, frequency: float, value: complex):
+        self.k = k                # índice discreto
+        self.frequency = frequency # frecuencia asociada (Hz)
+        self.value = value         # coeficiente complejo
+
+    @property
+    def magnitude(self) -> float:
+        return abs(self.value)
+
+    @property
+    def phase(self) -> float:
+        return cmath.phase(self.value)
+
+    def __repr__(self):
+        return (f"FourierComponent(k={self.k}, f={self.frequency:.2f} Hz, "
+                f"mag={self.magnitude:.3f}, phase={self.phase:.3f})")
+
+    def __to_dict__(self):
+        return {
+            "index[k]": self.k,
+            "frecuency": self.frequency,
+            "magnitude": self.magnitude,
+            "phase": self.phase
+        }
