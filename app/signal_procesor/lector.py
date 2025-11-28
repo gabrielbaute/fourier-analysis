cfea35aff5904fbed901e40afb66768d56169633
gabrielbaute <gabrielbaute@gmail.com>
2025-11-27 21:48:08 -0400

    Logging y modo streaming

--- app/signal_procesor/lector.py
+++ app/signal_procesor/lector.py
@@ -1,9 +1,12 @@
 import serial
+import logging
+import matplotlib.pyplot as plt
 
 class Signal:
-    def __init__(self, baudrate: int = 115200, port: str = "COM3"):
+    def __init__(self, baudrate: int = 115200, port: str = "COM15"):
         self.port = port
         self.baudrate = baudrate
+        self.logger = logging.getLogger(f"{self.__class__.__name__}")
         self.ser = serial.Serial(self.port, self.baudrate, timeout=1)
 
     def read_line(self) -> str:
@@ -17,7 +20,9 @@ class Signal:
         if linea.startswith("[") and linea.endswith("]"):
             # elimina corchetes y separa por coma
             valores = linea[1:-1].split(",")
-            return [float(v.strip()) for v in valores]
+            response = [float(v.strip()) for v in valores]
+            self.logger.info(f"Valores obtenidos: {len(response)} muestras")
+            return response
         return []
 
     def write_command(self, cmd: str):
@@ -27,6 +32,22 @@ class Signal:
     def available(self) -> int:
         """Devuelve cuántos bytes hay en el buffer"""
         return self.ser.in_waiting
+
+    def stream(self):
+        """Modo streaming: lee continuamente y grafica en tiempo real"""
+        plt.ion()
+        fig, ax = plt.subplots()
+
+        while True:
+            valores = self.read_values()
+            if valores:
+                ax.clear()
+                ax.plot(valores, marker="o")
+                ax.set_ylim(0, 3.3)
+                ax.set_title("Streaming ADC RP2040")
+                ax.set_xlabel("Muestra")
+                ax.set_ylabel("Voltaje (V)")
+                plt.pause(0.01)
 
     def close(self) -> bool:
         try:
