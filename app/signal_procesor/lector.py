ad9604c562581a31c7479b0204c20d703193dd64
gabrielbaute <gabrielbaute@gmail.com>
2025-12-02 21:28:26 -0400

    Logging mejorado. Instancia directa de Serial en el constructor

--- app/signal_procesor/lector.py
+++ app/signal_procesor/lector.py
@@ -1,22 +1,23 @@
-import serial
 import logging
 import matplotlib.pyplot as plt
+from serial import Serial
+from typing import List
 
 class Signal:
     def __init__(self, baudrate: int = 115200, port: str = "COM15"):
         self.port = port
         self.baudrate = baudrate
         self.logger = logging.getLogger(f"{self.__class__.__name__}")
-        self.ser = serial.Serial(self.port, self.baudrate, timeout=1)
+        self.ser = Serial(self.port, self.baudrate, timeout=1)
 
-    def read_line(self) -> str:
+    def _read_line(self) -> str:
         """Lee una línea cruda desde el puerto serial y la devuelve como string"""
         raw = self.ser.readline()
         return raw.decode("utf-8").strip()
 
-    def read_values(self) -> list[float]:
+    def read_values(self) -> List[float]:
         """Convierte la línea recibida en lista de floats"""
-        linea = self.read_line()
+        linea = self._read_line()
         if linea.startswith("[") and linea.endswith("]"):
             # elimina corchetes y separa por coma
             valores = linea[1:-1].split(",")
@@ -27,16 +28,20 @@ class Signal:
 
     def write_command(self, cmd: str):
         """Envía un comando al Pico"""
+        self.logger.info(f"Enviando comando: {cmd}")
         self.ser.write((cmd + "\n").encode("utf-8"))
 
     def available(self) -> int:
         """Devuelve cuántos bytes hay en el buffer"""
+        self.logger.info("Consultando bytes disponibles en buffer")
         return self.ser.in_waiting
 
     def stream(self):
         """Modo streaming: lee continuamente y grafica en tiempo real"""
         plt.ion()
         fig, ax = plt.subplots()
+
+        self.logger.info("Iniciando modo streaming...")
 
         while True:
             valores = self.read_values()
@@ -52,7 +57,8 @@ class Signal:
     def close(self) -> bool:
         try:
             self.ser.close()
+            self.logger.info("Puerto serial cerrado correctamente.")
             return True
         except Exception as e:
-            print(f"Error al cerrar el puerto serial: {e}")
+            self.logger.error(f"Error al cerrar el puerto serial: {e}")
             return False
